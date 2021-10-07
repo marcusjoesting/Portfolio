62022b264a948c9eb85f8641967d90eff686674e
marcusjoesting <marcusjoesting@gmail.com>
2021-10-07 11:38:44 -0400

    Updated styling and wording

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -21,19 +21,19 @@ export default function HeroSection(props) {
         <Section>
 
         <div className="relative min-h-screen flex flex-col md:flex-row px-5 md:px-10  text-black shadow-lg w-full justify-center items-center align-middle font-mono z-10">
-        <   div className="md:w-1/2 w-full h-screen">
+                <div className="md:w-1/2 w-full h-screen">
                   <Laptop/>
                 </div>
         
     
             <Fade left delay={500}>
-                <div className=" backdrop-filter backdrop-blur-lg filter py-10 relative flex flex-col md:w-1/2 w-full font-sans font-bold space-y-5 md:space-y-10">
-                
-            
-               
-                <div className='w-full flex flex-wrap space-y-10'>
-                    <h1 className="w-full text-4xl text-white">
+                <div className=" backdrop-filter backdrop-blur-lg filter relative flex flex-col md:w-1/2 w-full font-sans font-bold space-y-5 ">
+                <div className='w-full  p-4 flex flex-wrap'>
+                    <h1 className="w-full text-7xl text-white">
                         Full Stack Software Engineer
+                    </h1>
+                    <h1 className="w-full text-xl text-black mb-10 py-5">
+                        I understand problems and solve them to a high standard.
                     </h1>
                 <Link to="Projects" spy={true} smooth={true} duration={500} className="pr-2">
                 <Button alt>
